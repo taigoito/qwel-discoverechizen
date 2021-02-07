e311e5cc4bdc067a4cff0be2bff763e728684a8d
Taigo Ito <webmaster@qwel.design>
2021-02-08 04:26:25 +0900

    Pin specific announcements

--- parts/sections/news.php
+++ parts/sections/news.php
@@ -14,8 +14,20 @@
         <div class="cols__container">
           <?php
           $posts = get_posts([
-            'posts_per_page' => 4,
+            'posts_per_page' => 3,
+            'category' => 1,
             'post_type' => 'post'
+          ]);
+          foreach ($posts as $post) {
+            setup_postdata($post);
+            get_template_part('parts/components/cols');
+          }
+          ?>
+          <?php
+          $posts = get_posts([
+            'posts_per_page' => 1,
+            'post_type' => 'post',
+            'include' => [2080]
           ]);
           foreach ($posts as $post) {
             setup_postdata($post);
@@ -24,6 +36,6 @@
           ?>
         </div>
       </div><!-- .cols -->
-      <a class="news__more" href="<?php the_permalink(2); ?>">>&gt; お知らせ をもっと見る</a>
+      <a class="news__more" href="<?php echo get_category_link(1); ?>">&gt; 最新情報 をもっと見る</a>
     </section><!-- .news -->
     
