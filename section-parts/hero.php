3c38063778a14e631996383fc0852b265b479b1e
Taigo Ito <webmaster@qwel.design>
2022-06-13 11:05:24 +0900

    Fix the preloader

--- section-parts/hero.php
+++ section-parts/hero.php
@@ -3,7 +3,7 @@
     $section_id        = $section;
     $section_class     = 'section --' . $section_id;
     ?>
-    <header id="<?php echo $section_id; ?>" class="<?php echo $section_class; ?>">
+    <header id="<?php echo $section_id; ?>" class="<?php echo $section_class; ?> preloader" data-img-src="<?php echo DE_THEME_ASSETS . 'illust_logo_large.svg'; ?>">
       <div class="section__content">
         <img class="hero__logo" src="<?php echo DE_THEME_ASSETS . 'illust_logo_large.svg'; ?>"> 
       </div>
--- template-parts/cover.php
+++ template-parts/cover.php
@@ -1,7 +1,7 @@
   <?php
   if (is_front_page()) {
   ?>
-    <div class="cover preloader" data-background-color="#18aada" data-img-src="<?php echo DE_THEME_ASSETS . 'illust_logo_large.svg'; ?>">
+    <div class="cover">
       <div class="fader">
         <?php for ($i = 1; $i <= 6; $i++) { ?>
           <div class="cover__image --num_<?php echo $i; ?>"></div>
