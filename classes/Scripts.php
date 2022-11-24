13ad83e81d8bce4bd60cb02a2cb2bf4d05a3181a
Taigo Ito <webmaster@qwel.design>
2022-11-24 15:42:28 +0900

    Setup theme.json to apply theme colors and fonts

--- classes/Scripts.php
+++ classes/Scripts.php
@@ -8,6 +8,18 @@ trait Scripts {
 		$version = wp_get_theme()->get( 'Version' );
 		$version = is_string( $theme_version ) ? $theme_version : false;
 
+		// Google Fonts
+    $family = 'Sawarabi+Gothic';
+    if ($family !== '') {
+      wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css?family=' . $family . '&display=swap', [], null); 
+    }
+
+    // Typesquare
+    /*$url = '//typesquare.com/3/tsst/script/ja/typesquare.js?61b823f8fd9c446dacda0a37ac1e02e5&fadein=10';
+    if ($url !== '') {
+      wp_enqueue_script('typesquare-fonts', $url, [], null, false);
+    }*/
+
 		// style.css
 		wp_enqueue_style(
 			'style',
