7fbe27be2c0265903f09b7e358266b4e7cff39c1
Taigo Ito <webmaster@qwel.design>
2022-11-29 11:36:19 +0900

    Specify media upload size

--- classes/Supports.php
+++ classes/Supports.php
@@ -10,6 +10,17 @@ trait Supports {
 		// エディターにstyle.cssをセット
 		add_editor_style( 'style.css' );
 
+    // メディアサイズ指定
+    // large-sizeが個別投稿の幅一杯
+    update_option('thumbnail_size_w', 192);
+    update_option('thumbnail_size_h', 192);
+    update_option('medium_size_w', 384);
+    update_option('medium_size_h', 384);
+    update_option('medium_large_size_w', 0);
+    update_option('medium_large_size_h', 0);
+    update_option('large_size_w', 768);
+    update_option('large_size_h', 768);
+    
   }
 
 }
